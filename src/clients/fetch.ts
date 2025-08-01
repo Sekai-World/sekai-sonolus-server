3bea8e9e90a200af5a0709c2e37b16f45a6a1e04
Burrito <burritoyangster@gmail.com>
2025-08-02 05:42:05 +0800

    Fix aborting in whitelist (#61)

--- src/clients/fetch.ts
+++ src/clients/fetch.ts
@@ -1,28 +1,30 @@
 import { config as _config } from '../config.js'
 import { ClientConfig } from './config.js'
 
-export const fetchJson = async <T>(path: string, config: ClientConfig) => {
-    const response = await _fetch('GET', path, config)
+export const fetchJson = async <T>(path: string, config: ClientConfig, signal?: AbortSignal) => {
+    const response = await _fetch('GET', path, config, signal)
     return (await response.json()) as T
 }
 
-export const fetchText = async (path: string, config: ClientConfig) => {
-    const response = await _fetch('GET', path, config)
+export const fetchText = async (path: string, config: ClientConfig, signal?: AbortSignal) => {
+    const response = await _fetch('GET', path, config, signal)
     return await response.text()
 }
 
-export const fetchHead = async (path: string, config: ClientConfig) => {
-    const response = await _fetch('HEAD', path, config)
+export const fetchHead = async (path: string, config: ClientConfig, signal?: AbortSignal) => {
+    const response = await _fetch('HEAD', path, config, signal)
     return response.ok
 }
 
-const _fetch = (method: string, path: string, config: ClientConfig) => {
+const _fetch = (method: string, path: string, config: ClientConfig, signal?: AbortSignal) => {
     const url = `${config.baseUrl}${path}`
 
     if (_config.verbose) console.log(method, url)
 
     return fetch(url, {
         method,
-        signal: AbortSignal.timeout(config.timeout),
+        signal: signal
+            ? AbortSignal.any([signal, AbortSignal.timeout(config.timeout)])
+            : AbortSignal.timeout(config.timeout),
     })
 }
