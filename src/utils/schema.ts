a2b43ab4178c1fc42cb3d07b7e5c89aaf0eed799
Burrito <burritoyangster@gmail.com>
2026-03-31 22:54:38 +0800

    Renovate (#83)

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -2,5 +2,6 @@ import { TLiteral, TLiteralValue, TUnion, Type } from '@sinclair/typebox'
 
 export const toArrayEnumSchema = <T extends readonly TLiteralValue[]>(values: T) =>
     Type.Union(values.map((value) => Type.Literal(value))) as TUnion<{
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments
         -readonly [K in keyof T]: TLiteral<T[K]>
     }>
