4d71b15f0cda919f5259283e11e36890653fde32
NonSpicyBurrito <burritoyangster@gmail.com>
2026-05-27 15:08:47 +0800

    Refactored to use interpolation

--- src/sonolus/utils/i18n.ts
+++ src/sonolus/utils/i18n.ts
@@ -1,19 +1,28 @@
 import { LocalizationText, localize } from '@sonolus/core'
+import { locales } from '../../clients/i18n/locale.js'
 import { config } from '../../config.js'
-import { mapValues } from '../../utils/object.js'
-import { format as _format } from '../../utils/string.js'
 
-export const format = (template: LocalizationText, values: LocalizationText[]) =>
-    mapValues(template, (locale, template) =>
-        _format(
-            template,
-            values.map((value) => localize(value, locale, config.sonolus.fallbackLocale)),
-        ),
+export const format = (template: string, values: LocalizationText[]) =>
+    Object.fromEntries(
+        Object.keys(locales).map((locale) => {
+            let text = template
+            for (const [index, value] of values.entries()) {
+                text = text.replace(
+                    `{${index}}`,
+                    localize(value, locale, config.sonolus.fallbackLocale),
+                )
+            }
+
+            return [locale, text]
+        }),
     )
 
-export const join = (separator: LocalizationText, values: LocalizationText[]) =>
-    mapValues(separator, (locale, separator) =>
-        values
-            .map((value) => localize(value, locale, config.sonolus.fallbackLocale))
-            .join(separator),
+export const join = (separator: string, values: LocalizationText[]) =>
+    Object.fromEntries(
+        Object.keys(locales).map((locale) => [
+            locale,
+            values
+                .map((value) => localize(value, locale, config.sonolus.fallbackLocale))
+                .join(separator),
+        ]),
     )
